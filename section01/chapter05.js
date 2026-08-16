f68d9656d542331bb3527114574734dae17550e6
고명재 <rhaudwo9331@gmail.com>
2026-08-16 15:36:23 +0900

    first commit

---
+++ section01/chapter05.js
@@ -0,0 +1,29 @@
+//1. Number Type
+let num1 = 10;
+let num2 = 3.14;
+let num3 = -20;
+
+let inf = Infinity;
+let mInf = -Infinity;
+
+let nan = NaN;
+
+// 2. String Type
+let myName = '고명재';
+let myLocation = '연수';
+let introduce = myName + myLocation;
+
+let introduceText = `${myName}는 ${myLocation}에 살고 있습니다.`;
+
+// 템플릿 리터럴 문법
+
+//3. Boolean Type
+let isSwitchOn = true;
+let isEmpty = false;
+
+//4. Null Type
+let empty = null;
+
+//5. Undefined Type
+let none;
+console.log(none); //undefined
